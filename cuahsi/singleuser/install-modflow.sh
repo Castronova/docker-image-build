455c5b382e68775f063b005af1b6cf340c879c22
Castronova <Castronova.Anthony@gmail.com>
2018-05-22 13:51:49 -0400

    added modflow build and test scripts. modified taudem test script

--- cuahsi/singleuser/install-modflow.sh
+++ cuahsi/singleuser/install-modflow.sh
@@ -6,14 +6,25 @@ set -e
 echo 1 | update-alternatives --config gcc
 echo 1 | update-alternatives --config g++
 
-# compile MODFLOW 2005
 cd /tmp
-wget https://water.usgs.gov/ogw/modflow/MODFLOW-2005_v1.12.00/MF2005.1_12u.zip
-unzip -x MF2005.1_12u.zip
-cd MF2005.1_12u/make
-sed -i '/F90FLAGS*/F90FLAGS = -fno-second-underscore/'
-sed -i '/CC =*/CC = gcc/'
+wget https://water.usgs.gov/ogw/modflow/mf6.0.2.zip
+unzip -x mf6.0.2.zip
+cp /tmp/makefile /tmp/mf6.0.2/make
+cd /tmp/mf6.0.2/make
 make
-cp mf2005 /usr/local/bin/mf2005
+cp /tmp/mf6.0.2/make/mf6.0.2 /usr/local/bin
+
+
+## compile MODFLOW 2005
+#cd /tmp
+#wget https://water.usgs.gov/ogw/modflow/MODFLOW-2005_v1.12.00/MF2005.1_12u.zip
+#unzip -x MF2005.1_12u.zip
+#cd MF2005.1_12u/make
+#sed -i '/F90FLAGS/c\F90FLAGS = -fno-second-underscore' makefile
+#sed -i '/CC =*/c\CC = gcc' makefile
+##sed -i '/F90FLAGS*/F90FLAGS = -fno-second-underscore/'
+##sed -i '/CC =*/CC = gcc/'
+#make
+#cp mf2005 /usr/local/bin/mf2005
 
 rm -rf /tmp/*
