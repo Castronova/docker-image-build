3430f8ecacba09388827d96ad6eaf1d16698d517
Castronova <Castronova.Anthony@gmail.com>
2018-05-30 11:05:31 -0400

    added dplyer, sf, ncdf4, and dataretrieval R libraries

--- cuahsi/singleuser/install-r.sh
+++ cuahsi/singleuser/install-r.sh
@@ -7,18 +7,23 @@ set -e
 ####################################
 
 mkdir /home/jovyan/.userRLib
+echo "options(repos=structure(c(CRAN=\"http://archive.linux.duke.edu/cran\")))" >> /home/jovyan/.Rprofile
+conda config --add channels r
 
 conda install -n R -c r \
-    r-base=3.4.3 \
-    r-essentials=3.4.3 \
-    r-devtools=1.13.4 \
-    r-xml=3.98_1.9 \
-    r-rjsonio=1.3_0 \
-    && conda clean --all -y
+    r-base=3.4.1 \
+    r-irkernel \
+    r-essentials \
+    r-devtools \
+    r-xml \
+    r-rjsonio \
+    r-ncdf4 \
+    r-sf
 
-echo "options(repos=structure(c(CRAN=\"http://archive.linux.duke.edu/cran\")))" >> /home/jovyan/.Rprofile
-#Rscript -e "install.packages('devtools', repos='http://archive.linux.duke.edu/cran')" 
-Rscript -e "IRkernel::installspec(name = 'ir34', displayname = 'R 3.4')" 
 Rscript -e "install.packages('WaterML')" 
+Rscript -e "install.packages('dplyr')" 
+Rscript -e "install.packages('dataRetrieval',repos='https://owi.usgs.gov/R')"
+Rscript -e "install.packages('stringi')"
+Rscript -e "IRkernel::installspec(name = 'ir34', displayname = 'R 3.4')" 
 
 conda clean --all -y
